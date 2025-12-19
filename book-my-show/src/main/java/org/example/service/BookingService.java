da491b92e94501fd4b7f7b82fe5012ce27a5b9fc
Girish Narayanan <girishnarayanan@yahoo.com>
2025-12-19 19:23:12 +0530

    Transitioned seat status to Locked before payment step

--- book-my-show/src/main/java/org/example/service/BookingService.java
+++ book-my-show/src/main/java/org/example/service/BookingService.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.entity.Booking;
+import org.example.entity.Seat;
 import org.example.repository.BookingRepository;
 import org.example.repository.SeatRepository;
 import org.example.visitors.DiscountVisitor;
@@ -9,7 +10,9 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,8 +43,29 @@ public class BookingService {
         for (DiscountVisitor visitor: visitors){
             visitor.apply(booking);
         }
-        booking.getBookingSeats().parallelStream().forEach(bookingSeat -> bookingSeat.getSeat().setStatus("BOOKED"));
-        return bookingRepository.saveAndFlush(booking);
+        List<Seat> seatsToBeBooked = booking.getBookingSeats()
+                .parallelStream()
+                .map(mapper->mapper.getSeat())
+                .map(mapper1->{
+                    mapper1.setStatus("LOCKED");
+                    return mapper1;
+                })
+                .collect(Collectors.toList());
+        seatRepository.saveAllAndFlush(seatsToBeBooked);
+        CompletableFuture<String> paymentFuture = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Payment Successful";
+        });
+        if(paymentFuture.isDone()) {
+            booking.getBookingSeats().parallelStream().forEach(bookingSeat -> bookingSeat.getSeat().setStatus("BOOKED"));
+            return bookingRepository.saveAndFlush(booking);
+        } else{
+            throw new RuntimeException("Payment was unsuccessful. Please try again.");
+        }
     }
 
     public Booking getBooking(String id) {
